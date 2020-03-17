3b6f0c240ba1e9ba831acbcb1382e154e9cf9e60
josezea1 <josezea@gmail.com>
2020-03-17 11:22:39 -0500

    prueba

---
+++ precache-manifest.639532580b3ddaa3a07a1ed7d189b197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d9c45248951cded0e3feef42105f47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0567caee3fc51db87a56",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6a6f0a090ae893149b54",
+    "url": "/static/js/2.303272dd.chunk.js"
+  },
+  {
+    "revision": "86f96943d6537349861c4002a14026e3",
+    "url": "/static/js/2.303272dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0567caee3fc51db87a56",
+    "url": "/static/js/main.133f6850.chunk.js"
+  },
+  {
+    "revision": "8320f497263db686d718",
+    "url": "/static/js/runtime-main.681eba5a.js"
+  }
+]);
